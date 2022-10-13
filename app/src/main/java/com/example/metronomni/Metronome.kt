6dd8736027db8047db88e5123e0f8dcdc0672248
tcollins6049 <collinstm@appstate.edu>
2022-10-12 20:58:07 -0500

    Sound works but cuts off with error.

--- app/src/main/java/com/example/metronomni/Metronome.kt
+++ app/src/main/java/com/example/metronomni/Metronome.kt
@@ -2,11 +2,14 @@ package com.example.metronomni
 import kotlin.concurrent.timerTask
 import java.util.*
 import android.util.Log
+import android.media.ToneGenerator
+import android.media.AudioManager
 
 
 
 object Metronome {
     private var metronome: Timer
+    private const val METRONOME_TONE = ToneGenerator.TONE_PROP_BEEP
 
     init {
         metronome = Timer("metronome", true)
@@ -17,9 +20,29 @@ object Metronome {
     }
 
     fun startMet(bpm: Int): Boolean {
+        //To calculate times between tasks
+        /*
+        var startTime = System.currentTimeMillis()
+        var prevTime = startTime
         this.metronome.schedule(
             timerTask {
-                Log.i("met", "Tick")
+                prevTime = startTime
+                startTime = System.currentTimeMillis()
+                var total = startTime - prevTime
+                Log.i("met", "$total")
+            },
+            0L,
+            calculateSleepPeriod(bpm)
+        )
+        return true
+        */
+
+        // To play sound
+        this.metronome.schedule(
+            timerTask {
+                val toneGenerator = ToneGenerator(AudioManager.STREAM_MUSIC, 100)
+                toneGenerator.startTone(METRONOME_TONE)
+                //toneGenerator.release()
             },
             0L,
             calculateSleepPeriod(bpm)
